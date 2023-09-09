490fd6e26e4ca09b4de507345454128f1c34657c
tarekomar96 <tarekomar28014@gmail.com>
2023-09-09 15:45:30 +0300

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",redirectTo:"about",pathMatch:"full"},
+  {path:"about",component:AboutComponent},
+  {path:"portfolio",component:PortfolioComponent},
+  {path:"contact",component:ContactComponent},
+  {path:"**",component:NotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import { InformationComponent } from './information/information.component';
+import { FooterComponent } from './footer/footer.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    InformationComponent,
+    FooterComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
